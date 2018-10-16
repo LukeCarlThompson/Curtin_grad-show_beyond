0e54452374bb80c52a4101f8b3451cb25d34dde5
Luke Carl Thompson <info@lukethompson.com.au>
2018-10-16 19:24:44 +0800

    removed post thumbnails from theme

--- functions.php
+++ functions.php
@@ -40,7 +40,7 @@ if ( ! function_exists( 'beyond_setup' ) ) :
 		 *
 		 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
 		 */
-		add_theme_support( 'post-thumbnails' );
+		// add_theme_support( 'post-thumbnails' );
 
 		// This theme uses wp_nav_menu() in one location.
 		register_nav_menus( array(
